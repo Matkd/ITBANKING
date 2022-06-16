c69273eaa6ff7c14982b0280cf81f5be9c6fb699
Santiago Chaves <santiagochaves242@gmail.com>
2022-06-16 20:31:03 -0300

    Cotizaciones

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-// Bars Nav
 const bars = document.querySelector('.bars');
+// Bars Nav
 const navLinks = document.querySelector('.nav-links');
 const navBtns = document.querySelector('.nav-btns');
 
@@ -15,3 +15,46 @@ bars.addEventListener('click', () => {
   navLinks.classList.add('links-active');
   navBtns.classList.add('btns-active');
 });
+
+// Cotizaciones
+const url = 'https://www.dolarsi.com/api/api.php?type=valoresprincipales';
+const container = document.querySelector('.cotizacion');
+
+function getDatos() {
+  fetch(url)
+    .then(data => data.json())
+    .then(elems => elems.filter(elem => elem.casa.venta !== '0'))
+    .then(elems => elems.filter(elem => elem.casa.variacion !== undefined))
+    .then(res => {
+      let b = 1;
+      res.forEach(elem => {
+        let div = `
+      <div class="cotizacion-card">
+        <div class="cotizacion-header">
+          <div class="cotizacion-icon">
+            <img src="Img\\cotizacion\\b${b}.png" alt="" />
+          </div>
+          <h3 class="cotizacion-titulo">${elem.casa.nombre}</h3>
+        </div>
+        <div class="cotizacion-body">
+      `;
+        b++;
+
+        if (elem.casa.compra !== 'No Cotiza') {
+          div += `<p>COMPRA: $${elem.casa.compra}</p>`;
+        }
+
+        div += `
+          <P>VENTA: $${elem.casa.venta}</P>
+        </div>
+        <div class="cotizacion-footer">
+          <P>VARIACIÓN: ${elem.casa.variacion}%</P>
+        </div>
+      </div>
+      `;
+        container.innerHTML += div;
+      });
+    });
+}
+
+getDatos();
